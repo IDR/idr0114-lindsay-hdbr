cec82431ff4a3bc577bf2f5966902f1a83c447a9
Petr Walczysko <p.walczysko@dundee.ac.uk>
2021-10-05 22:07:53 +0100

    Add logic for detection of duplicate image names

--- make-tsv.py
+++ make-tsv.py
@@ -13,6 +13,7 @@
 # python upload_attachments.py
 
 import csv
+import collections
 
 rows = []
 image_names = []
@@ -21,6 +22,7 @@ match = 0
 full_match = 0
 match_count = 0
 no_match_count = 0
+repeat_check = ""
 annot_file = "./experimentA/from_annot_im_and_dat.tsv"
 attachment_file = "./experimentA/idr0114-experimentA-filePaths.tsv"    # "/tmp/attachments.txt"
 dataset_names = ["C127_H4K5ac", "C127_H3K36me2", "C127_RNAP-S2P", "C127_H3K4me2",
@@ -34,11 +36,12 @@ def process_line(line, count, match, rows):
     # /uod/idr/filesets/idr0082-pennycuick-lesions/20200417-ftp/S2_HandE.ndpi.ndpa
     parts = line.split('	')
     path = parts[1]
-    #print (count)
-    #print (image_name)
+    split_path = path.split('/')
+    image_name = split_path[len(split_path)-1]
+    image_names.append(image_name)
     paths.append(path)
     #image_names.append(image_name)
-    return paths
+    return image_names,paths
 
 def write_tsv(rows):
     with open('experimentA/test.tsv', mode='w') as tsv_file:
@@ -53,12 +56,13 @@ with open(attachment_file) as fp:
 
         #print("Line %d" % count)
         #print (line)
-        (paths) = process_line(line, count, 0, rows)
+        (image_names,paths) = process_line(line, count, 0, rows)
         line = fp.readline().strip()
 #print ("match_count")
 #print (match_count)
 #print ("no match_count")
 #print (no_match_count)
+
 count3 = 0
 for path in paths:
     #print(image_name)
@@ -83,5 +87,6 @@ for path in paths:
                 rows.append(['Project:name:idr0114-lindsay-hdbr/experimentA/Dataset:name:'+dataset_name, path])
                 break
             line = ap.readline().strip()
+print([item for item, count in collections.Counter(image_names).items() if count > 1])
 write_tsv(rows)
 
