9341deb30390f57aceb9377397d45b5bfb46d57a
Petr Walczysko <p.walczysko@dundee.ac.uk>
2021-10-06 10:26:10 +0100

    Clean the code, remove print statements

--- make-tsv.py
+++ make-tsv.py
@@ -24,23 +24,16 @@ match_count = 0
 no_match_count = 0
 repeat_check = ""
 annot_file = "./experimentA/from_annot_im_and_dat.tsv"
-attachment_file = "./experimentA/idr0114-experimentA-filePaths.tsv"    # "/tmp/attachments.txt"
-dataset_names = ["C127_H4K5ac", "C127_H3K36me2", "C127_RNAP-S2P", "C127_H3K4me2",
-                 "C127_H3K27me3", "C127_H3K9me2", "C127_SAF-A", "C127_Smc3", "C127_CTCF", "C127_Macro-H2A", 
-                 "C127_H3K4me3", "C127_aHP1", "C127_Scc1", "C127_SCC1","C127_H3K36me3", "C127_H4K20me1",
-                 "C127_30min-BrUTP", "C127_H3K9me3", "C127_H4K20me3", "C127_hnRNP", "HeLa_Rad21",
-                 "HeLa_H3K4me3", "HeLa_H3K9me3", "HeLa_H3K27me3", "HeLa_hnRNPC", "HeLa_RNAP-S2P", "HeLa_Scc1", "HeLa_RNAPIIS2P"]
+attachment_file = "./experimentA/idr0114-experimentA-filePaths.tsv"
+duplicate_names = ['3DView.avi', '3DView.jpg']
 
 def process_line(line, count, match, rows):
-    # /uod/idr/filesets/idr0082-pennycuick-lesions/20200417-ftp/S1_HandE.ndpi.ndpa
-    # /uod/idr/filesets/idr0082-pennycuick-lesions/20200417-ftp/S2_HandE.ndpi.ndpa
     parts = line.split('	')
     path = parts[1]
     split_path = path.split('/')
     image_name = split_path[len(split_path)-1]
     image_names.append(image_name)
     paths.append(path)
-    #image_names.append(image_name)
     return image_names,paths
 
 def write_tsv(rows):
@@ -53,19 +46,11 @@ with open(attachment_file) as fp:
     count = 0
     while line and len(line) > 0:
         count += 1
-
-        #print("Line %d" % count)
-        #print (line)
         (image_names,paths) = process_line(line, count, 0, rows)
         line = fp.readline().strip()
-#print ("match_count")
-#print (match_count)
-#print ("no match_count")
-#print (no_match_count)
 
 count3 = 0
 for path in paths:
-    #print(image_name)
     count3 += 1
     with open(annot_file) as ap:
         line = ap.readline().strip()
@@ -73,17 +58,14 @@ for path in paths:
         bits = path.split('/')
         lenght = len(bits)
         image_name = bits[lenght - 1]
+        dupe = False
+        if image_name in duplicate_names:
+            dupe = True
+            image_name = bits[lenght-2]+'\\'+ image_name
         while line and len(line) > 0:
             count2 += 1
             dataset_name = line.split('	')[0]
-            print ("dataset name")
-            print (dataset_name)
             if image_name in line:
-                print(count3)
-                print(image_name)
-                print(count2)
-                print(line)
-                print("match")
                 rows.append(['Project:name:idr0114-lindsay-hdbr/experimentA/Dataset:name:'+dataset_name, path])
                 break
             line = ap.readline().strip()
